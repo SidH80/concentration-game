0e9781b263834196ae1c4d50cb4c583b41e2c299
lethalSid <senien.hicks@gmail.com>
2019-11-01 09:18:21 -0400

    Updates

---
+++ precache-manifest.0c9d2fd468d154e7e70b2144edf84ad8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b56a789936b243d009f72ef747c6065",
+    "url": "/Concentration-Game/index.html"
+  },
+  {
+    "revision": "4b233249aeb8d8c1d8c0",
+    "url": "/Concentration-Game/static/css/main.b9408f3c.chunk.css"
+  },
+  {
+    "revision": "11ba3183c7b75d6f7088",
+    "url": "/Concentration-Game/static/js/2.a8bd4251.chunk.js"
+  },
+  {
+    "revision": "4b233249aeb8d8c1d8c0",
+    "url": "/Concentration-Game/static/js/main.b36fa2ff.chunk.js"
+  },
+  {
+    "revision": "fef5a267395e67d596ad",
+    "url": "/Concentration-Game/static/js/runtime-main.d90eca46.js"
+  }
+]);
